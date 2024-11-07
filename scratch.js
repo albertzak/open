a16f18d2e444fad49223d7851f15814d3dfb9e40
Albert Zak <security@fixpoint.co.at>
2024-11-07 22:00:00 +0100

    460

--- scratch.js
+++ scratch.js
@@ -5,7 +5,7 @@ let c = mq.connect('wss://q.fxp.at:443')
 c.on('error', (e) => console.error(e))
 c.on('message', (m) => console.log('msg', m))
 
-c.subscribe('t', (...e) => {
+c.subscribe('#', (...e) => {
   console.log('sub', e)
   c.publish('t', 'hi')
 })
