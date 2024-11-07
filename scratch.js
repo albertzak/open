4e1fe3c86e22d8928acce0eca2d31106e29070fc
Albert Zak <security@fixpoint.co.at>
2024-11-07 21:13:00 +0100

    456

---
+++ scratch.js
@@ -0,0 +1,14 @@
+import mq from 'mqtt'
+
+let c = mq.connect('wss://q.fxp.at')
+
+c.on('error', (e) => console.error(e))
+c.on('message', (m) => console.log('msg', m))
+
+c.subscribe('t', (...e) => {
+  console.log('sub', e)
+  c.publish('t', 'hi')
+})
+
+
+
