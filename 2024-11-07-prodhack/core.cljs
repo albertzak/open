cde66d2a13c436d2b51ceffa9bdec9c620490883
Albert Zak <security@fixpoint.co.at>
2024-11-10 21:36:00 +0100

    484

--- 2024-11-07-prodhack/core.cljs
+++ 2024-11-07-prodhack/core.cljs
@@ -1,4 +1,19 @@
 (ns prodhack.core
-  (:require ["ws" :as ws]))
+  (:require ["ws" :refer [WebSocketServer]]))
 
-(prn ws)
+(defn log [& xs]
+  (apply prn xs)
+  (last xs))
+
+(defn on-connection [^js ws]
+  (.on ws "error" (partial log :error))
+  (.on ws "message"
+       (fn [data]
+         (log :ws-message data)))
+  (.send ws "hello"))
+
+(defonce wss
+  (doto (WebSocketServer.
+         #js {:port 8080})
+    (.on "connection" #'on-connection)))
+
